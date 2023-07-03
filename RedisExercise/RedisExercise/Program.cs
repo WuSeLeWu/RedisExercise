b63a42c8ff049aba9fe1595ee12bb6be8db48aa5
WuSeLeWu <91565862+WuSeLeWu@users.noreply.github.com>
2023-07-03 20:10:12 +0300

    RedisExercise
    Redis Exercise

---
+++ RedisExercise/RedisExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using RedisExercise;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var redis = new Redis("ADIGE\\SQLEXPRESS"); //Veri Tabanı yolumuzu Redis sınıfına veriyoruz.
+
+        // Veri ekleme işlemleri
+        redis.AddData("name", "Mustafa");
+        redis.AddData("lastname", "YILMAN");
+        redis.AddData("age", "21");
+        redis.AddData("delete", "delete");
+
+
+        // Veri getirme işlemleri
+        string name = redis.GetData("name");
+        string lastname = redis.GetData("lastname");
+        string age = redis.GetData("age");
+        string delete = redis.GetData("delete");
+
+        Console.WriteLine(
+            $" Adı: {name}\n" +
+            $" Soyadı: {lastname}\n" +
+            $" Yasi: {age}"
+        );
+
+        // Veri silme işlemleri
+        redis.DeleteData("delete");
+
+
+        /*      Console Görünümü
+         * 
+         * Adı : Mustafa
+         * Soyadı : YILMAN
+         * Yasi : 21
+         * 
+         * 
+         */
+    }
+}
---
+++ RedisExercise/RedisExercise/Redis.cs
@@ -0,0 +1,47 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisExercise
+{
+    public class Redis
+    {
+        private IDatabase Database;
+
+        //Redis sınıfının nesnesini oluştururken bağlantı yolunun verilmesini mecbur kılıyoruz ve yapıcı blok içerisinde bağlantı kodlarını yazıyoruz.
+        public Redis(string connectionPath)
+        {
+            var connectionOptions = new ConfigurationOptions
+            {
+                EndPoints = { connectionPath },
+                AbortOnConnectFail = false
+            };
+
+            ConnectionMultiplexer a = ConnectionMultiplexer.Connect(connectionOptions);
+            Database = a.GetDatabase();
+        }
+
+        //Veri eklemek için StringSet() metodunu kullanıyoruz bunu daha kolay yapabilmek ve sürekli kullanabilmek için metot halinde yaptık.
+        public void AddData(string key, string value)
+        {
+            Database.StringSet(key, value);
+        }
+
+        //Veri getirmek için StringGet() metodunu kullanıyoruz bu metot anahtar üzerinden değeri getiriyor.
+
+        public string GetData(string key)
+        {
+            return Database.StringGet(key);
+        }
+
+        //Veri silebilmek için KeyDelete() metodunu kullanıyoruz bu metot sayesinde anahtar üzerinde değeri silebiliyoruz.
+
+        public void DeleteData(string key)
+        {
+            Database.KeyDelete(key);
+        }
+    }
+}
